4e8d5088d8195ffa33c516a97362ebd71662a418
Josh Pollock <jpollock412@gmail.com>
2014-12-04 19:23:55 -0500

    pull in core files from modularity branch

--- plugincore.php
+++ plugincore.php
@@ -3,16 +3,16 @@
  * @package   Metaplate
  * @author    David <david@digilab.co.za>
  * @license   GPL-2.0+
- * @link      
+ * @link
  * @copyright 2014 David
  *
  * @wordpress-plugin
  * Plugin Name: Metaplate
- * Plugin URI:  
+ * Plugin URI:
  * Description: Create Meta Templates to display Custom Fields and Post Meta
  * Version:     1.0.0
  * Author:      David
- * Author URI:  
+ * Author URI:
  * Text Domain: metaplate
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -25,17 +25,31 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 define('MTPT_PATH',  plugin_dir_path( __FILE__ ) );
-define('MTPT_URL',  plugin_dir_url( __FILE__ ) );
+define('MTPT_URL',  plugin_dir_url( __FILE__ ) .'includes/caldera/metaplate' );
 define('MTPT_VER',  '1.0.0' );
 
 //autoload dependencies uncomment after running composer update
 require_once( MTPT_PATH . 'vendor/autoload.php' );
 
-// load internals
-require_once( MTPT_PATH . 'core-class.php' );
-require_once( MTPT_PATH . 'includes/helpers.php' );
-require_once( MTPT_PATH . 'includes/settings.php' );
+
 
 // Load instance
-add_action( 'plugins_loaded', array( 'Metaplate', 'get_instance' ) );
-//Metaplate::get_instance();
+add_action( 'plugins_loaded', function() {
+	require_once( MTPT_PATH . 'core-class.php' );
+	new Metaplate();
+
+}, 13 );
+
+
+//Temporary psr-4 autoloader for now
+//@todo rm
+add_action( 'plugins_loaded', function(){
+	include_once( MTPT_PATH .'includes/classloader.php' );
+	$class_loader = new Caldera_MetaPlate_Autoloader();
+	$class_loader->addNamespace( 'caldera', MTPT_PATH . 'includes/caldera' );
+
+	$class_loader->register();
+	// load internals
+
+}, 11 );
+
